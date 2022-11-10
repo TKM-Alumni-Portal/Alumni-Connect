6df4f5ebd412118eeb70dc5afbd7c6be40e41df7
joyal-jkp <94290371+joyal-jkp@users.noreply.github.com>
2022-11-10 15:12:11 +0530

    settings/mediaurl

--- AlumniPortal/AlumniPortal/settings.py
+++ AlumniPortal/AlumniPortal/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = 'alumnis/static/media/'
+MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'alumnis/static/media')
 
